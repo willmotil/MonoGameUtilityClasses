0e516162764575b58e6212ce542e4ded2c1b134c
willmotil <willmotil@users.noreply.github.com>
2018-05-04 15:26:56 -0400

    Update SurfaceMesh.cs

--- SurfaceMesh.cs
+++ SurfaceMesh.cs
@@ -3,7 +3,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace SplineSurfaceDemo
+namespace Microsoft.Xna.Framework
 {
     /// <summary>
     /// Will Motill 2016.
